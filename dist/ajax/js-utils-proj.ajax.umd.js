469dbdab2872b780d0670f43df47c61f307ad846
charescape <charescape@outlook.com>
2021-09-24 16:31:39 +0800

    build

--- dist/ajax/js-utils-proj.ajax.umd.js
+++ dist/ajax/js-utils-proj.ajax.umd.js
@@ -2,7 +2,7 @@
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
     typeof define === 'function' && define.amd ? define(['exports'], factory) :
     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.JsUtilsProj_Ajax = {}));
-}(this, (function (exports) { 'use strict';
+})(this, (function (exports) { 'use strict';
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
@@ -168,4 +168,4 @@
 
     Object.defineProperty(exports, '__esModule', { value: true });
 
-})));
+}));
